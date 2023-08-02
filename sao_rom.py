4f00f9ac721edea189c4d0ee33c5a67a4a6a8da6
RobotMan2412 <julian@scheffers.net>
2023-08-02 20:58:05 +0200

    Fix the SAO ROM writer up a bit

--- sao_rom.py
+++ sao_rom.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 # Firefly SAO ROM generator
 
+import machine, time
+
 rom = [
     0x4c, 0x49, 0x46, 0x45, 0x0e, 0x07, 0x06, 0x02,
     0x46, 0x69, 0x72, 0x65, 0x66, 0x6c, 0x79, 0x20,
@@ -20,23 +22,26 @@ def format_rom(serial):
     serial = int(serial)
     return rom + num_to_arr(serial)
 
-def print_rom_writer(custom_rom):
-    print("import machine")
+def write_rom(custom_rom):
+    # Writing
     for i in range(0, len(custom_rom), 64):
         snippet = num_to_arr(i) + custom_rom[i:i+64]
-        print("machine.I2C(0).writeto(0x50, {})".format(repr(bytes(snippet))))
-
-def _print_help():
-    print("Usage: sao_rom.py <Serial No.>")
-    print("Outputs an MCH2022 python SAO formatting command")
-
-if __name__ == "__main__":
-    from sys import argv
-    if len(argv) != 2:
-        _print_help()
+        machine.I2C(0).writeto(0x50, bytes(snippet))
+        time.sleep(0.1)
+    # Verification
+    machine.I2C(0).writeto(0x50, b'\x00\x00')
+    if machine.I2C(0).readfrom(0x50, len(custom_rom)) == bytes(custom_rom):
+        print("\n\nROM validated!")
+        return True
     else:
-        try:
-            custom = format_rom(int(argv[1]))
-            print_rom_writer(custom)
-        except:
-            _print_help()
+        print("\n\nROM mismatch!")
+        return False
+
+def format_and_write(serial, tries=3):
+    custom_rom = format_rom(serial)
+    for i in range(tries):
+        if write_rom(custom_rom):
+            return
+        else:
+            print("Attempt {}/{} failed".format(i+1, tries))
+            time.sleep(0.5)
